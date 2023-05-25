ab990a56b780fa2f0f812b7660979ec198fc4fb0
Asahi Kiyuna <s21009@std.it-college.ac.jp>
2023-05-25 12:38:33 +0900

    lesson3-1 extra

---
+++ Chapter3/Lesson3-3/linked_list_1_3.py
@@ -0,0 +1,76 @@
+MAX = 7
+data = [None]*MAX
+pointer = [None]*MAX
+head = 0
+
+def add_list(d):
+    n = -1
+    for i in range(MAX):
+        if data[i] == None:
+            n = i
+            break
+    if n == -1:
+        print("データ領域に空きがありません")
+        return False
+    for i in range(MAX):
+        if data[i] != None and pointer[i] == None:
+            pointer[i] = n
+            break
+    data[n] = d
+    pointer[n] = None
+    print(f'データ {d} を追加しました')
+    return True
+
+def del_list(d):
+    global head
+    n = -1
+    for i in range(MAX):
+        if data[i] == d:
+            n = i
+            break
+    if n == -1:
+        print('そのデータは存在しません')
+        return False
+    if n != head:
+        for i in range(MAX):
+            if pointer[i] == n:
+                pointer[i] = pointer[n]
+    else:
+        head = pointer[n]
+        if head == None:
+            head = 0
+    data[n] = None
+    pointer[n] = None
+    print(f'データ {d} を削除しました')
+    return True
+
+def put_list():
+    p = head
+    while True:
+        print(data[p], end=' → ')
+        if pointer[p] == None:
+            print('EOF')
+            break
+        p = pointer[p]
+
+add_list('赤')
+add_list('橙')
+add_list('黄')
+add_list('緑')
+add_list('青')
+add_list('藍')
+add_list('紫')
+put_list()
+del_list('藍')
+add_list('藍')
+del_list('青')
+add_list('青')
+del_list('緑')
+add_list('緑')
+del_list('黄')
+add_list('黄')
+del_list('橙')
+add_list('橙')
+del_list('赤')
+add_list('赤')
+put_list()
